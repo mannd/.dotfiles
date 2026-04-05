3da4610437aa7605a80d52ffda8da5da29805201
David Mann <mannd@epstudiossoftware.com>
2026-04-05 15:07:28 -0600

    Configuration.org now has errors

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,38 +1,31 @@
 ;;; init.el --- Initialization file for Emacs
-
 ;;; Author: David Mann
-
 ;;; Commentary:
 
 ;; Most of the actual initing happens in ~/.emacs.d/configuration.org
 
 ;;; Code:
-
 ;; Time Emacs startup.
-
 (defconst emacs-start-time (current-time))
 (unless noninteractive
   (message "Loading %s..." load-file-name))
 
+;; Make sure the package system is initialized.
+(setq package-enable-at-startup nil)
+(require 'package)
+
+(setq package-archives
+      '(("gnu" . "https://elpa.gnu.org/packages/")
+        ("melpa" . "https://melpa.org/packages/")))
+
+(package-initialize)
+
+;; Load org (package version)
+(require 'org)
+(require 'ob-tangle)
+
 ;; Load my literate Emacs configuration.
 (org-babel-load-file "~/.emacs.d/configuration.org")
-;; (let* ((org-file "~/.emacs.d/configuration.org")
-;;        (el-file  "~/.emacs.d/configuration.el")
-;;        (elc-file "~/.emacs.d/configuration.elc"))
-;;   ;; Tangle if .org is newer than .el
-;;   (when (file-newer-than-file-p org-file el-file)
-;;     (require 'org)
-;;     (org-babel-tangle-file org-file el-file))
-
-;;   ;; Byte-compile if .el is newer than .elc
-;;   (when (file-newer-than-file-p el-file elc-file)
-;;     (byte-compile-file el-file))
-
-;;   ;; Load the compiled version if it exists, otherwise the plain .el
-;;   (if (file-exists-p elc-file)
-;;       (load elc-file)
-;;     (load el-file)))
-
 
 ;; Finish timing the startup.
 ;; THIS NEEDS TO BE LAST IN init.el.
